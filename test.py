69821f4d52caee44a6b468d43147f49e046a3f9a
Matus Pikuliak <matus.pikuliak@gmail.com>
2018-04-10 09:32:34 +0200

    .

--- test.py
+++ test.py
@@ -7,6 +7,28 @@ config = Config()
 cache = Cache(config)
 cache = cache.load()
 
+# dt = cache.fetch_dataset('ner', 'en', 'train')
+# unk = cache.lang_dicts['en'][1]['<unk>']
+# zer = cache.task_dicts['ner'][1]['O']
+# all = 0
+# all_u = 0
+# ner = 0
+# ner_u = 0
+# for s in dt.samples:
+#     for i in xrange(len(s[0])):
+#         all += 1
+#         if s[0][i] == unk:
+#             all_u += 1
+#         if s[1][i] != zer:
+#             ner += 1
+#             if s[0][i] == unk:
+#                 ner_u += 1
+# print all, all_u, ner, ner_u
+#
+#
+#
+# exit()
+
 def run_test(train, cache, config, epoch):
     name = ' '.join([' '.join(t) for t in train])
     with open('./logs.txt', 'a') as f:
