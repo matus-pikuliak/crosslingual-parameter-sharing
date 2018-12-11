83d7dbc8a1b6e131819f91225144ed4bedc5d2be
matus-pikuliak <matus.pikuliak@stuba.sk>
2018-12-11 17:44:18 +0100

    Result handling overhaul

---
+++ runs.py
@@ -0,0 +1,28 @@
+'''
+11.12.2018 martak run
+'''
+
+ls = ['cs', 'de', 'en', 'es']
+ts = ['dep', 'lmo', 'ner', 'pos']
+
+for t in ts:
+    for l in ls:
+        print(f'bash train.sh tasks {t}-{l} && ', end='')
+
+for t in ts:
+    print(f'bash train.sh tasks ', end='')
+    for l in ls:
+        print(f'{t}-{l} ', end='')
+    print('&& ', end='')
+
+for l in ls:
+    print(f'bash train.sh tasks ', end='')
+    for t in ts:
+        print(f'{t}-{l} ', end='')
+    print('&& ', end='')
+
+print(f'bash train.sh tasks ', end='')
+for l in ls:
+    for t in ts:
+        print(f'{t}-{l} ', end='')
+print('&& ', end='')
