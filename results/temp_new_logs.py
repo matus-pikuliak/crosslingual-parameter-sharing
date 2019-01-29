39af64063ad4f103d6654d9af8fa942ac73559d7
matus-pikuliak <matus.pikuliak@stuba.sk>
2019-01-29 21:53:11 +0100

    generate new logs from existing models

--- results/temp_new_logs.py
+++ results/temp_new_logs.py
@@ -5,19 +5,19 @@ import sys
 
 sys.path.append(os.path.abspath('..'))
 from config.config import Config
+from evaluate import evaluate
 
 old_log_path = sys.argv[1]
-model_path = ...
-new_log_path = ...
+model_path = sys.argv[2]
+new_log_path = sys.argv[3]
 
 for log_file in glob.glob(os.path.join(old_log_path, '*')):
     if os.path.isfile(log_file):
+        _, run_name = os.path.split(log_file)
         with open(log_file) as log_f:
             contents = ast.literal_eval(log_f.read())
             config = Config()
             config.load_from_logfile(contents['hparams'])
-            print(config.values)
-    # import hparams
-    # change paths
-    # load models and run evaluation with them
-
+            config.values['log_path'] = new_log_path
+            config.values['model_path'] = model_path
+            evaluate(run_name, config)
