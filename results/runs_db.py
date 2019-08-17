b9affb23da3cf7b20385a8a1e56f3ff0be9eda55
matus-pikuliak <matus.pikuliak@gmail.com>
2019-08-17 14:08:54 +0200

    new runs

--- results/runs_db.py
+++ results/runs_db.py
@@ -7,6 +7,13 @@ db = {
         (12, 'rel', 'zero-shot'),
         (12, 'rel', 'zero-shot-task-lang'),
         (12, 'rel', 'zero-shot-task-lang-no-global'),
+        (12, 'all', 'zero-shot-task-lang-ortho-50'),
+        (12, 'all', 'zero-shot-task-lang-no-global-ortho-50'),
+        (12, 'all', 'zero-shot-task-lang-ortho-100'),
+        (12, 'all', 'zero-shot-task-lang-no-global-ortho-100'),
+        (12, 'all', 'zero-shot-task-lang-ortho-200'),
+        (12, 'all', 'zero-shot-task-lang-no-global-ortho-200'),
+        
     ],
     'deepnet2070': [
         (12, 'ml-3', 'zero-shot'),
